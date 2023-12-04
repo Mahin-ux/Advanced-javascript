5a38aabc397dc04bfb94750a3175158d60d46468
Mahin-ux <mahinhossain962@gmail.com>
2023-12-04 20:28:44 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,48 @@
+// const ages = 4
+
+// if(ages){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false")
+// }
+
+
+const name = true
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false")
+}
+
+
+let name1
+console.log(name1)
+
+
+
+let name2 = NaN
+console.log(name2)
+
+let name3 = null
+console.log(name3)
+
+//truety:
+//" "
+//"0"
+//{}
+//[]
+//"false"
+//true
+//"true"
+
+
+//falsy:
+//false
+//""
+//undefind
+//NaN
+//null
+//0
